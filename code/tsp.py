b7e4d5dc169e98396296569b62240d0966ea138f
alex-gl <alexgolovnev@Alexs-MacBook-Air-2.local>
2018-08-14 14:43:43 -0400

    tsp for paths with a fixed start vertex

--- code/tsp.py
+++ code/tsp.py
@@ -1,4 +1,6 @@
 from itertools import chain, combinations
+import math
+import networkx as nx
 
 
 # This function takes as input a graph g.
@@ -10,59 +12,100 @@ def powerset(s):
     return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
-# This function finds an optimal Hamiltonian path using the dynamic programming approach.
-def dynamic_programming(g):
+# This function finds uses the dynamic programming approach
+# to find an optimal Hamiltonian path starting at the vertex start.
+def dynamic_programming(g, begin):
     # n is the number of vertices.
     n = g.number_of_nodes()
 
-    # The variable power now contains a tuple for each subset of the set {0, ..., n-1}.
-    power = powerset(range(n))
-    # The variable T is a dictionary, where the element T[s, begin, end] for a set s and integers
-    # begin and end equals the shortest path going through each vertex from s exactly once,
-    # starting at the vertex begin and ending at the vertex end.
-    # Note that begin and end must be in s.
+    # The variable power now contains a tuple for each subset
+    # of the set {0, ..., n-1} \ {begin}.
+    power = powerset([x for x in range(n) if x != begin])
+    # The variable T is a dictionary, where the element T[s, end] for a set s and an integer
+    # end equals the shortest path going through each vertex from s exactly once,
+    # starting at the vertex start and ending at the vertex end.
+    # Thus, for convenience, we will always exclude the element begin from the set s.
+    # Note that end must be in s.
     # In order to reconstruct an optimal solution, we also store the values of parents:
-    # p[s, begin, end] is the last vertex before end in an optimal path through s.
+    # p[s, end] is the last vertex before end in an optimal path going through s.
     T = {}
     p = {}
-    # For every pair of distinct vertices i and j, we set
-    # T[ tuple with the two element i and j, i, i] to the weight of the arc from i to j.
-    for begin in range(n):
-        for end in range(n):
-            if begin != end:
-                T[(min(begin, end), max(begin, end)), begin, end] = g[begin][end]['weight']
-                p[(min(begin, end), max(begin, end)), begin, end] = begin
+    # For every vertex end != start we set T[ tuple with the element end only, end]
+    # to the weight of the edge from start to end.
+    for end in range(n):
+        if end != begin:
+            # Syntactic note: In Python, we define a tuple of length 1
+            # that contains the element end as (end,) *with comma*.
+            T[(end,), end] = g[begin][end]['weight']
+            p[(end,), end] = begin
 
-    # For every subset s of [0,...,n-1]
+    # For every subset s of {0,...,n-1} \ {begin}
     for s in power:
         # We have already initialized the elements of T indexed by sets of size 1, so we skip them.
-        if len(s) > 2:
-            # For every vertex start from s which we consider as the starting vertex
+        if len(s) > 1:
+            # For every vertex end from s which we consider as the ending vertex
             # of a path going through vertices from s.
-            for start in s:
-                # For every vertex end from s which we consider as the ending vertex
-                # of a path going through vertices from s.
-                for end in s:
-                    if start != end:
-                        # Define the tuple which contains all elements from s without *the last vertex* end.
-                        t = tuple([x for x in s if x != end])
-                        # Now we compute the optimal value of a path which visits all vertices from s,
-                        #  and ends at the vertex end.
-                        mn, idx = min((T[t, start, i] + g[i][end]['weight'], i) for i in t if i != start)
-                        T[s, start, end] = mn
-                        p[s, start, end] = idx
+            for end in s:
+                if begin != end:
+                    # Define the tuple which contains all elements from s without *the last vertex* end.
+                    t = tuple([x for x in s if x != end])
+                    # Now we compute the optimal value of a path which visits all vertices from s,
+                    #  and ends at the vertex end.
+                    mn, idx = min((T[t, i] + g[i][end]['weight'], i) for i in t)
+                    T[s, end] = mn
+                    p[s, end] = idx
 
     # Compute the weight of on optimal Hamiltonian path.
-    opt, start, finish = min((T[tuple(range(n)), begin, end], begin, end)
-                             for begin in range(n) for end in range(0, n) if end != begin)
-    # Return an optimal Hamiltonian path.
+    # all is the set of all vertices except begin.
+    all = tuple([x for x in range(n) if x != begin])
+    opt, finish = min((T[all, end], end) for end in range(n) if end != begin)
+
+    # Compute an optimal Hamiltonian path.
     path = []
     node = finish
-    s = tuple(range(n))
-    while node != start:
+    s = all
+    while node != begin:
         path.append(node)
-        next = p[s, start, node]
+        next = p[s, node]
         s = tuple([x for x in s if x != node])
         node = next
-    path.append(start)
-    return list(reversed(path))
+    path.append(begin)
+    return opt, list(reversed(path))
+
+
+coordinates = {0: [(178, 212), (287, 131), (98, 156)],
+                   1: [(231, 91), (7, 21), (226, 276), (11, 266)],
+                   2: [(174, 25), (129, 99), (268, 212), (211, 209), (156, 82)],
+                   3: [(199, 59), (152, 117), (68, 87), (281, 161), (11, 53), (254, 227)],
+                   4: [(44, 105), (137, 277), (53, 202), (93, 296), (156, 214), (187, 137), (147, 195)],
+                   5: [(162, 137), (122, 177), (249, 49), (37, 127), (13, 277), (164, 293), (270, 42), (135, 123)],
+                   6: [(165, 117), (13, 241), (97, 260), (287, 299), (273, 19), (138, 22), (120, 25), (10, 286),
+                       (95, 150)],
+                   7: [(88, 106), (248, 67), (251, 24), (124, 221), (136, 148), (262, 88), (179, 45), (60, 188),
+                       (272, 99), (30, 107)],
+                   8: [(181, 243), (101, 143), (100, 216), (167, 15), (37, 201), (163, 226), (2, 42), (35, 73),
+                       (85, 116), (142, 235), (200, 18)]}
+
+
+def dist(x1, y1, x2, y2):
+        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def path_length(g, path):
+        assert len(path) == g.number_of_nodes()
+        return sum(g[path[i]][path[i + 1]]['weight'] for i in range(len(path) - 1))
+
+
+def read_graph(test_index):
+        g = nx.DiGraph()
+        coors = coordinates[test_index]
+        n = len(coors)
+        for i in range(n):
+            for j in range(n):
+                g.add_edge(i, j, weight=dist(coors[i][0], coors[i][1], coors[j][0], coors[j][1]))
+        return g
+
+
+g = read_graph(0)
+dynamic_programming(g, 0)
+
